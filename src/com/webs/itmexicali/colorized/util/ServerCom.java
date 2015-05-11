0f6aee3f94228f1f2d70350ef2d2e97d5975838b
Michael Duarte <mduarte@brooklynpacket.com>
2015-05-10 23:52:00 -0700

    clean ServerCom code

--- src/com/webs/itmexicali/colorized/util/ServerCom.java
+++ src/com/webs/itmexicali/colorized/util/ServerCom.java
@@ -20,25 +20,22 @@ public class ServerCom extends server.ServerCom{
 		}
 		return instance;
 	}
-
-
-
+	
 	@Override
 	public boolean isNetworkAvailable() {
 		boolean connected = false;
 		ConnectivityManager cm = (ConnectivityManager) GameActivity.instance.getSystemService(Context.CONNECTIVITY_SERVICE);
-		
-		//this method iterates thru all the networks available to check which one is connected
-		NetworkInfo[] netInfo = cm.getAllNetworkInfo();
-		for (NetworkInfo ni : netInfo) 
-			if ( ni.isConnected() /*&& ni.getTypeName().equalsIgnoreCase("WIFI")*/ )
-				connected = true;
-		
-		//this method gets the active network and checks if it is connected
-		NetworkInfo activeNetworkInfo = cm.getActiveNetworkInfo();
-		connected = activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
-		
-		
+		if ( cm != null ){
+			//this method iterates thru all the networks available to check which one is connected
+	//		NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+	//		for (NetworkInfo ni : netInfo) 
+	//			if ( ni.isConnected() /*&& ni.getTypeName().equalsIgnoreCase("WIFI")*/ )
+	//				connected = true;
+			
+			//this method gets the active network and checks if it is connected
+			NetworkInfo activeNetworkInfo = cm.getActiveNetworkInfo();
+			connected = activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+		}
 		return connected;
 	}
 }
