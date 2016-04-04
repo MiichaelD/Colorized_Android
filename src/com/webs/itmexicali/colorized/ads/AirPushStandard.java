7981963e7f275d557bba9e80ab3729bc7c54bb0d
MiichaelD <michael.duarte@hotmail.com>
2016-04-04 00:16:22 -0700

    Updated airpush lib

--- src/com/webs/itmexicali/colorized/ads/AirPushStandard.java
+++ src/com/webs/itmexicali/colorized/ads/AirPushStandard.java
@@ -3,6 +3,12 @@ package com.webs.itmexicali.colorized.ads;
 import android.app.Activity;
 import android.view.View;
 
+import com.xsqhbao.bipppts201390.AdConfig;
+import com.xsqhbao.bipppts201390.AdConfig.AdType;
+import com.xsqhbao.bipppts201390.AdListener;
+import com.xsqhbao.bipppts201390.AdView;
+import com.xsqhbao.bipppts201390.Main;
+
 import com.webs.itmexicali.colorized.util.Log;
 
 /** Advertising by AirPush - Standard edition 
@@ -12,12 +18,15 @@ import com.webs.itmexicali.colorized.util.Log;
  * NOTE: to use this ads, you should uncomment some lines of code in
  * AirPushStandard class and the AndroidManifest file(THE PROPER MODIFICATIONS 
  * OF MANIFEST ARE AVAILABLE AT THE END OF THIS CLASS)*/
+
+
 public class AirPushStandard extends Advertising{
 	
 
 	//AirPush Ads STD
-    private com.xsqhbao.bipppts201390.AdView AirPushView;
-    private com.xsqhbao.bipppts201390.Prm air_std;
+    private AdView airPushView;
+    private Main airMain;
+    private AdListener airListener;
 	
 	public AirPushStandard(Activity act) {
 		super(act);
@@ -25,76 +34,121 @@ public class AirPushStandard extends Advertising{
     
 	@Override
 	public void initAds() {
-		AirPushView = new com.xsqhbao.bipppts201390.AdView(
-				pAct, com.xsqhbao.bipppts201390.AdView.BANNER_TYPE_IN_APP_AD,
-				com.xsqhbao.bipppts201390.AdView.PLACEMENT_TYPE_INLINE, false, false, 
-				com.xsqhbao.bipppts201390.AdView.ANIMATION_TYPE_LEFT_TO_RIGHT);
+		airListener = new AirPushListener();
 		
-		air_std = new com.xsqhbao.bipppts201390.Prm(pAct,
-				new com.xsqhbao.bipppts201390.AdListener(){
-			@Override
-			public void noAdAvailableListener() {
-				Log.e(this.getClass().getSimpleName(), "noAdAvailableListener");	
-			}
-			@Override
-			public void onAdCached(com.xsqhbao.bipppts201390.AdListener.AdType arg0) {
-				Log.d(this.getClass().getSimpleName(), "onAdCached type: "+arg0.name());
-			}
-			@Override
-			public void onAdError(String arg0) {
-				Log.e(this.getClass().getSimpleName(), "onAdError: "+arg0);	
-			}
-			@Override
-			public void onSDKIntegrationError(String arg0) {
-				Log.e(this.getClass().getSimpleName(), "onSDKIntegrationError: "+arg0);	
-			}
-			@Override
-			public void onSmartWallAdClosed() {
-				Log.d(this.getClass().getSimpleName(), "onSmartWallAdClosed");
-				loadInterstitial();
-			}
-			@Override
-			public void onSmartWallAdShowing() {
-				Log.d(this.getClass().getSimpleName(), "onSmartWallAdShowing");				
-			}
-		},
-		true);
+		AdConfig.setAppId(Advertising.APPID);
+		AdConfig.setApiKey(Advertising.APIKEY);
+		AdConfig.setCachingEnabled(true);
+		AdConfig.setPlacementId(0);  //AdView.PLACEMENT_TYPE_INLINE); // what does this mean???
+		AdConfig.setAdListener(airListener);
+
+		airMain = new Main(pAct);
+		
+		airPushView = new AdView(pAct);
+		airPushView.setBannerType(AdView.BANNER_TYPE_IN_APP_AD);
+		airPushView.setBannerAnimation(AdView.ANIMATION_TYPE_LEFT_TO_RIGHT);
+		airPushView.showMRinInApp(false);
+		airPushView.setNewAdListener(airListener);
+		airPushView.loadAd(); 
 		
 		//360 Ad.
-		//air_std.run360Ad(pAct,0,false,null);
+		//air_main.run360Ad(pAct);
 		
 		loadInterstitial();
 	}
 
 	@Override
 	public View getBanner() {
-		return AirPushView;
+		return airPushView;
 	}
 	@Override
 	public void destroyBanner() {
-		if(AirPushView != null){// remove the existing
-        	AirPushView = null;
+		if(airPushView != null){// remove the existing
+        	airPushView = null;
         }
 	}
 	@Override
 	public void loadInterstitial() {
 		//Caching Smartwall Ad for future use
-		air_std.runSmartWallAd();
-		//air_std.runAppWall();
+		airMain.startInterstitialAd(AdType.smartwall);
 	}
 	
 	@Override
 	public void showInterstitial() {
 		try {
-			air_std.runCachedAd(pAct, com.xsqhbao.bipppts201390.AdListener.AdType.smartwall);
-			//air_std.runCachedAd(pAct, com.xsqhbao.bipppts201390.AdListener.AdType.appwall);
+			airMain.showCachedAd(AdType.smartwall);
 		} catch (Exception e) {
-			Log.w(this.getClass().getSimpleName(),"Interstitial not showing Cached");
-			air_std.runSmartWallAd();
+			Log.w(this.getClass().getSimpleName(),"showInterstitial(): Interstitial not showing Cached");
+			loadInterstitial();
 			e.printStackTrace();
 		}
 	}
+	
+	class AirPushListener implements AdListener{
+		@Override
+		public void onAdError(String errorMessage) {
+			//This will get called if any error occurred during ad serving.
+			Log.e(this.getClass().getSimpleName(), "onAdError(): "+errorMessage);	
+		}
+		@Override
+		public void noAdListener() {
+			 //this will get called when ad is not available 
+			Log.d(this.getClass().getSimpleName(), "noAdListener()");
+			loadInterstitial();
+		}
+		@Override
+		public void onAdCached(AdType adType) {
+			//This will get called when an ad is cached. 
+			Log.d(this.getClass().getSimpleName(), "onAdCached(): "+adType.name());	
+		}
+		@Override
+		public void onAdClickedListener() {
+			 //This will get called when ad is clicked.
+			Log.d(this.getClass().getSimpleName(), "onAdClickedListener()");	
+		}
+		@Override
+		public void onAdClosed() {
+			// This will be called by SDK when the SmartWall ad is closed.
+			Log.d(this.getClass().getSimpleName(), "onAdClosed()");
+			// SDK complains if we request a smart wall in less than 10 secs after showing it
+//			loadInterstitial(); 
+		}
+		@Override
+		public void onAdExpandedListner() {
+			 //This will get called when an ad is showing on a user's screen. This may cover the whole UI.
+			Log.d(this.getClass().getSimpleName(), "onAdExpandedListener()");
+		}
+		@Override
+		public void onAdLoadedListener() {
+			  //This will get called when an ad has loaded.
+			Log.d(this.getClass().getSimpleName(), "onAdLoadedListener()");
+		}
+		@Override
+		public void onAdLoadingListener() {
+			 //This will get called when a rich media ad is loading.
+			Log.d(this.getClass().getSimpleName(), "onAdLoadingListener()");
+		}
+		@Override
+		public void onAdShowing() {
+			// This will get called by the sSDK when showing any of the smart wall ads
+			Log.d(this.getClass().getSimpleName(), "onAdShowing()");
+		}
+		@Override
+		public void onCloseListener() {
+			// when an ad is closing/resizing from an expanded state
+			Log.d(this.getClass().getSimpleName(), "onCloseListener()");
+		}
+		@Override
+		public void onIntegrationError(String errorMessage) {
+			//This will get called if any error occurred and if there are integration mistakes.
+			Log.w(this.getClass().getSimpleName(), "onIntegrationError(): "+errorMessage);
+		}
+	}
+	
 }
+
+
+
 /* **MANIFEST MODIFICATIONS:**
  
  ***********************	PERMISSIONS /	UNDER ROOT TAG: *******************************************
